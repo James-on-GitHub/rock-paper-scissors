2e6eb6e5c3b97451493dffdba651fff99435743f
James <172984924+James-on-GitHub@users.noreply.github.com>
2024-07-27 23:37:29 +1000

    Remove unnecessary for loop from getComputerChoice function

--- script.js
+++ script.js
@@ -1,9 +1,6 @@
 // Computers choice: will randomly return one of the array items when called.
 function getComputerChoice() {
     let option = ["Rock", "Paper", "Scissors"];
-    let choice = " ";
-    for (let i = 0; i < option.length; i++) {
-        choice = option[Math.floor(Math.random() * option.length)];
-    }
+    let choice = option[Math.floor(Math.random() * option.length)];
     return choice;
 }
