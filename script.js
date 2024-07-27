86ffb01ee5c472d8bc81d550f23f4569a06da603
James <172984924+James-on-GitHub@users.noreply.github.com>
2024-07-27 21:10:26 +1000

    Create getComputerChoice function

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-console.log("Hi James, you're awsome!");
+// Computers choice: will randomly return one of the array items when called.
+function getComputerChoice() {
+    let option = ["Rock", "Paper", "Scissors"];
+    let choice = " ";
+    for (let i = 0; i < option.length; i++) {
+        choice = option[Math.floor(Math.random() * option.length)];
+    }
+    return choice;
+}
