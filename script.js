ccbb3cde9163701ddf019f9db38406ea4f31ff6a
James <172984924+James-on-GitHub@users.noreply.github.com>
2024-07-28 16:21:58 +1000

    Add conditional statement to getHumanChoice function so that user input only accepts 'Rock', 'Paper' or 'Scissors'

--- script.js
+++ script.js
@@ -1,12 +1,21 @@
 // Computers choice: will randomly return one of the array items when called.
 function getComputerChoice() {
-    let option = ["Rock", "Paper", "Scissors"];
-    let choice = option[Math.floor(Math.random() * option.length)];
+    let options = ["Rock", "Paper", "Scissors"];
+    let choice = options[Math.floor(Math.random() * options.length)];
     return choice;
 }
 
 // Human choice: return user input.
 function getHumanChoice() {
+    let options = "Rock, Paper or Scissors."
     let choice = prompt("Type Rock, Paper or Scissors:");
-    return choice;
+    if (choice === "Rock") {
+        return choice;
+    } else if (choice === "Paper") {
+        return choice;
+    } else if (choice === "Scissors") {
+        return choice;
+    } else {
+        alert(`Please enter ${options}`);
+    }
 }
