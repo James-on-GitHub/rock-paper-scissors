43bb6dc53e725a345043862b96680941c44fd3d0
James <172984924+James-on-GitHub@users.noreply.github.com>
2024-07-29 12:57:04 +1000

    Add players score count to conditional 'if' statement in playround function

--- script.js
+++ script.js
@@ -12,18 +12,21 @@ function getHumanChoice() {
     let choice = prompt("Type Rock, Paper or Scissors:");
     if (choice === "Rock") {
         console.log(`You chose ${choice}.`);
-    } else if (choice === "Paper") {
+    } 
+    else if (choice === "Paper") {
         console.log(`You chose ${choice}.`);
-    } else if (choice === "Scissors") {
+    } 
+    else if (choice === "Scissors") {
         console.log(`You chose ${choice}.`);
-    } else {
+    } 
+    else {
         alert(`Please enter ${options}`);
     }
     return choice;
 }
 
 let humanScore = 0;
-let computerSore = 0;
+let computerScore = 0;
 const humanSelection = getHumanChoice();
 const computerSelection = getComputerChoice();
 const dualSelection = humanSelection && computerSelection;
@@ -31,24 +34,44 @@ const dualSelection = humanSelection && computerSelection;
 function playRound(humanChoice, computerChoice) {
     if (humanChoice === computerChoice) {
         console.log(`It's a draw! You both chose ${dualSelection}.`);
+        console.log(`You're score is ${humanScore}.`);
+        console.log(`The computers score is ${computerScore}.`);
     } 
     else if (humanChoice === "Rock" && computerChoice === "Paper") {
+        computerScore = ++computerScore;
         console.log(`You lose! ${computerChoice} beats ${humanChoice}.`);
+        console.log(`You're score is ${humanScore}.`);
+        console.log(`The computers score is ${computerScore}.`);
     } 
     else if (humanChoice === "Rock" && computerChoice === "Scissors") {
+        humanScore = ++humanScore;
         console.log(`You win! ${humanChoice} beats ${computerChoice}.`);
+        console.log(`You're score is ${humanScore}.`);
+        console.log(`The computers score is ${computerScore}.`);
     } 
     else if (humanChoice === "Paper" && computerChoice === "Rock") {
+        humanScore = ++humanScore;
         console.log(`You win! ${humanChoice} beats ${computerChoice}.`);
+        console.log(`You're score is ${humanScore}.`);
+        console.log(`The computers score is ${computerScore}.`);
     } 
     else if (humanChoice === "Paper" && computerChoice === "Scissors") {
+        computerScore = ++computerScore;
         console.log(`You lose! ${computerChoice} beats ${humanChoice}.`);
+        console.log(`You're score is ${humanScore}.`);
+        console.log(`The computers score is ${computerScore}.`);
     }
     else if (humanChoice === "Scissors" && computerChoice === "Rock") {
+        computerScore = ++computerScore;
         console.log(`You lose! ${computerChoice} beats ${humanChoice}.`);
+        console.log(`You're score is ${humanScore}.`);
+        console.log(`The computers score is ${computerScore}.`);
     }
     else if (humanChoice === "Scissors" && computerChoice === "Paper") {
+        humanScore = ++humanScore;
         console.log(`You win! ${humanChoice} beats ${computerChoice}.`);
+        console.log(`You're score is ${humanScore}.`);
+        console.log(`The computers score is ${computerScore}.`);
     }
 }
 
